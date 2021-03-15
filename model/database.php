cc3eedbb7b2169ef786fc356b19dbc9f2915f9a0
Francs Lage <francs.lage@gmail.com>
2021-03-14 20:49:37 -0700

    Update database.php
    Heroku connection added

--- model/database.php
+++ model/database.php
@@ -1,9 +1,10 @@
 <?php
-    $dataSourceName = 'mysql:host=localhost;dbname=todolist';
-    $user = 'root';
+    $dataSourceName = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=m9hrdx7mawh6mtur';
+    $user = 'jf60ce86hzd5yrp5';
+    $password = 'mpafrjoxiz9i7l7u';
 
     try {
-        $database = new PDO($dataSourceName, $user);
+        $database = new PDO($dataSourceName, $user, $password);
     }
     catch (PDOException $e) {
         $error_message = 'Database Error: ';
